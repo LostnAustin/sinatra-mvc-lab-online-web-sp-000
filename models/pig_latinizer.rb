0605667b97cfab9e2de0c0a6dea6df33a0377550
Chad Lengefeld <chadlengefeld@gmail.com>
2020-04-15 17:15:16 +0000

    Automatically backed up by Learn

--- models/pig_latinizer.rb
+++ models/pig_latinizer.rb
@@ -15,7 +15,7 @@ class PigLatinizer
   end
 
   def latinize(word)
-    alpha = ('a'..'z').to_a
+    alpha = ('a'..'z', 'A'..'Z').to_a
     vowels = %w[a e i o u]
     consonants = alpha - vowels
     a = []
